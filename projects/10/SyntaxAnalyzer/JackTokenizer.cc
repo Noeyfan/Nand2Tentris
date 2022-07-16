ca68944bced7d22ada24f9bb417838ab959112cc
fanyou <youfan.noey@gmail.com>
2022-07-15 17:16:42 -0700

    Project 10 Tokenizer Done.

---
+++ projects/10/SyntaxAnalyzer/JackTokenizer.cc
@@ -0,0 +1,173 @@
+#include <fstream>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+// enums
+enum KeyWords {
+    CLASS, METHOD, FUNCTION, CONSTRUCTOR, INT, BOOLEAN, CHAR, VOID, VAR, STATIC,
+    FIELD, LET, DO, IF, ELSE, WHILE, RETURN, TRUE, FALSE, JNULL, THIS
+};
+
+enum TokenTypes {
+    KEYWORD,
+    SYMBOL,
+    IDENTIFIER,
+    INT_CONST,
+    STRING_CONST,
+};
+
+class JackTokenizer {
+private:
+  // static
+  unordered_map<string, KeyWords> keywords = {
+        {"class", KeyWords::CLASS},
+        {"constructor", KeyWords::CONSTRUCTOR},
+        {"function", KeyWords::FUNCTION},
+        {"method", KeyWords::METHOD},
+        {"field", KeyWords::FIELD},
+        {"static", KeyWords::STATIC},
+        {"var", KeyWords::VAR},
+        {"int", KeyWords::INT},
+        {"char", KeyWords::CHAR},
+        {"boolean", KeyWords::BOOLEAN},
+        {"void", KeyWords::TRUE},
+        {"true", KeyWords::FALSE},
+        {"false", KeyWords::FALSE},
+        {"null", KeyWords::JNULL},
+        {"this", KeyWords::THIS},
+        {"let", KeyWords::LET},
+        {"do", KeyWords::DO},
+        {"if", KeyWords::IF},
+        {"else", KeyWords::ELSE},
+        {"while", KeyWords::WHILE},
+        {"return", KeyWords::RETURN},
+  };
+
+  unordered_set<char> symbols = {
+      '{', '}', '(', ')', '[', ']', '.', ',', ';', '+',
+      '-', '*', '/', '&', '|', '<', '>', '=', '~'
+  };
+
+  char identifierConcat = '_';
+  char stringBegin = '"', stringEnd = '"';
+
+  // member
+  ifstream& file;
+  string token;
+  TokenTypes tokenT;
+  bool moreToken;
+
+  // helper
+  void consume(ifstream& file, string& token) {
+      char c = file.get();
+      token += c;
+  }
+
+public:
+  JackTokenizer(ifstream& file):
+    file(file),
+    token("") {
+        advance();
+    }
+
+  bool hasMoreTokens() {
+      return moreToken;
+  }
+
+  void advance() {
+      token = "";
+      moreToken = false;
+      string discard;
+
+      while(file) {
+          char c = file.peek();
+          if (isalpha(c) || c == identifierConcat) {
+              consume(file, token);
+
+              while(isalpha(file.peek())
+                    || isdigit(file.peek())
+                    || file.peek() == identifierConcat) {
+                  consume(file, token);
+              }
+
+              tokenT = keywords.contains(token) ? KEYWORD: IDENTIFIER;
+              moreToken = true;
+              return;
+          } else if (isdigit(c)) {
+              consume(file, token);
+
+              while(isdigit(file.peek())) {
+                  consume(file, token);
+              }
+
+              tokenT = TokenTypes::INT_CONST;
+              moreToken = true;
+              return;
+          } else if (c == stringBegin) {
+              file.get(); // skip "
+
+              while(file.peek() != stringEnd) {
+                  if (c == '\n') {
+                      file.get(); // skip newline
+                      continue;
+                  }
+                  consume(file, token);
+              }
+              file.get(); // skip "
+
+              tokenT = TokenTypes::STRING_CONST;
+              moreToken = true;
+              return;
+          } else if (symbols.contains(c)) {
+              consume(file, token);
+
+              if (c == '/' && file.peek() == '/') {
+                  getline(file, discard);
+                  token = "";
+                  continue;
+              } else if (c == '/' && file.peek() == '*') {
+                  while(true) {
+                      c = file.get();
+                      if (c == '*' && file.peek() == '/') {
+                          file.get();
+                          break;
+                      }
+                  }
+                  token = "";
+                  continue;
+              }
+
+              tokenT = SYMBOL;
+              moreToken = true;
+              return;
+          } else {
+              file.get(); // skip unknown character
+          }
+      }
+  }
+
+  int tokenType() {
+      return tokenT;
+  }
+
+  int keyWord() {
+      return keywords[token];
+  }
+
+  char symbol() {
+      return token[0];
+  }
+
+  string identifier() {
+      return token;
+  }
+
+  int intVal() {
+      return stoi(token);
+  }
+
+  string stringVal() {
+      return token;
+  }
+
+};
